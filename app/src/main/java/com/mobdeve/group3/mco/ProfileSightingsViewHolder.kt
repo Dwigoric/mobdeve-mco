e4e45a559ce1520e311981e94b89510c893a4c36
Joshua Permito <git.commits@dwigoric.dev>
2024-10-19 13:41:31 +0800

    chore: prepend "Seen on" for profile sighting date

--- app/src/main/java/com/mobdeve/group3/mco/ProfileSightingsViewHolder.kt
+++ app/src/main/java/com/mobdeve/group3/mco/ProfileSightingsViewHolder.kt
@@ -5,10 +5,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
-class ProfileSightingsViewHolder(itemView: View): ViewHolder(itemView) {
+class ProfileSightingsViewHolder(itemView: View) : ViewHolder(itemView) {
     private val imgSighting: ImageView = itemView.findViewById(R.id.imgSighting)
     private val txtSightingName: TextView = itemView.findViewById(R.id.txtSightingName)
-    private val txtSightingNameScientific: TextView = itemView.findViewById(R.id.txtSightingNameScientific)
+    private val txtSightingNameScientific: TextView =
+        itemView.findViewById(R.id.txtSightingNameScientific)
     private val txtSighingPlace: TextView = itemView.findViewById(R.id.txtSightingPlace)
     private val txtSightingDate: TextView = itemView.findViewById(R.id.txtSightingDate)
 
@@ -17,6 +18,6 @@ class ProfileSightingsViewHolder(itemView: View): ViewHolder(itemView) {
         txtSightingName.text = sighting.name
         txtSightingNameScientific.text = sighting.scientificName
         txtSighingPlace.text = sighting.location
-        txtSightingDate.text = sighting.date
+        txtSightingDate.text = "Seen on ${sighting.date}"
     }
 }
