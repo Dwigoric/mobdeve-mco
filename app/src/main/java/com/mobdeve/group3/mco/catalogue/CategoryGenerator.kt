9c8e14779793f7dc0b893539939cffd2297826e4
Joshua Permito <git.commits@dwigoric.dev>
2024-10-21 15:37:03 +0800

    chore(catalogue): use drawable `fish` instead of `nemo`

--- app/src/main/java/com/mobdeve/group3/mco/catalogue/CategoryGenerator.kt
+++ app/src/main/java/com/mobdeve/group3/mco/catalogue/CategoryGenerator.kt
@@ -7,7 +7,14 @@ class CategoryGenerator {
         fun generateCategories(): ArrayList<Category> {
             val categories = ArrayList<Category>()
 
-            categories.add(Category("1", "Fish", R.drawable.nemo))
+            categories.add(Category("1", "Fish", R.drawable.fish))
+            categories.add(Category("2", "Mammals", R.drawable.mammals))
+            categories.add(Category("3", "Crustaceans", R.drawable.crustaceans))
+            categories.add(Category("4", "Mollusks", R.drawable.mollusks))
+            categories.add(Category("5", "Cnidarians", R.drawable.cnidarians))
+            categories.add(Category("6", "Echinoderms", R.drawable.echinoderms))
+            categories.add(Category("7", "Reptiles", R.drawable.reptiles))
+            categories.add(Category("8", "Birds", R.drawable.birds))
 
             return categories
         }
