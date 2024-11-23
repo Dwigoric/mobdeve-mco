f1f71aedc31b44a52b0bf66a719de375585ebdf7
Joshua Permito <git.commits@dwigoric.dev>
2024-11-23 13:43:08 +0800

    feat: move to MainActivity while signed in

--- app/src/main/java/com/mobdeve/group3/mco/landing/LandingActivity.kt
+++ app/src/main/java/com/mobdeve/group3/mco/landing/LandingActivity.kt
@@ -5,13 +5,22 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.mobdeve.group3.mco.MainActivity
 import com.mobdeve.group3.mco.R
 import com.mobdeve.group3.mco.databinding.ActivityLandingBinding
 
 class LandingActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        auth = Firebase.auth
 
         val viewBinding = ActivityLandingBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
@@ -27,6 +36,15 @@ class LandingActivity : AppCompatActivity() {
 
         btnRegister.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser != null) {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
